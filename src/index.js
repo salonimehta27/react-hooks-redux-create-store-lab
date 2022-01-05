1df2ab2b103b4389c3144427fc705617b2d49351
Saloni <salonimehta2127@gmail.com>
2022-01-05 01:46:46 -0500

    Completed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
+export function createStore(reducer) {
   // write your createStore code here
+  let state;
+
+  
+
+  function dispatch(action){
+    state=reducer(state,action)
+    render()
+  }
+
+  function getState(){
+    return state;
+  }
+
+  return{dispatch,
+    getState}
+  
+
 }
+
 
 export function candyReducer(state = [], action) {
   switch (action.type) {
@@ -13,6 +31,7 @@ export function candyReducer(state = [], action) {
   }
 }
 
+
 function render() {
   let container = document.getElementById("container");
   if (store.getState()) {
@@ -21,6 +40,7 @@ function render() {
     throw new Error("the store's state has not been defined yet");
   }
 }
-
+let store=createStore(candyReducer);
+store.dispatch({type:"@@INIT"})
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
